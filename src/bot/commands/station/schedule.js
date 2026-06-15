e58ad0aceafb4ae805be151c6a80a5a53b339ae6
n3m01726 <kiocubic.rulezz@proton.me>
2026-06-15 14:20:34 -0400

    fix: ajouter des logs de débogage pour le collecteur de planification

--- src/bot/commands/station/schedule.js
+++ src/bot/commands/station/schedule.js
@@ -60,6 +60,7 @@ export default {
 
       collector.on('collect', async (i) => {
         if (i.customId === 'schedule_fr') {
+          logger.debug(`Collector reçu: ${i.customId}`);
           await i.update({
             embeds: [
               new EmbedBuilder()
@@ -70,6 +71,7 @@ export default {
             components: []
           });
         } else if (i.customId === 'schedule_en') {
+          logger.debug(`Collector reçu: ${i.customId}`);
           await i.update({
             embeds: [
               new EmbedBuilder()
